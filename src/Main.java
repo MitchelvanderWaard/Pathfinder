88ee8af63308d93f9c5425604a3605af8c25bf27
Mitchel van der Waard <mitchelw62@hotmail.com>
2019-09-13 17:18:48 +0200

    new colors

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
     private GridPane root = new GridPane();
-    private int x = 5;
+    private int x = 10;
     private int y = 10;
 
     @Override
@@ -24,6 +25,7 @@ public class Main extends Application {
                 Rectangle n = new Rectangle();
                 n.setHeight(25);
                 n.setWidth(25);
+                n.setFill(Color.GRAY);
                 n.setOnMouseClicked(this::mouseSetColor);
                 root.add(n, a, i);
             }
@@ -35,19 +37,37 @@ public class Main extends Application {
         primaryStage.show();
 
 
-        aiSetColor(4,5);
+        aiSetColor(5,10);
 
 
     }
 
     private void mouseSetColor(MouseEvent mouseEvent) {
-        Rectangle t = (Rectangle) mouseEvent.getSource();
-        t.setFill(Color.BLUE);
+        Rectangle mouseTarget = (Rectangle) mouseEvent.getSource();
+        Paint mouseTargetColor = mouseTarget.getFill();
+        switch (mouseTargetColor.toString()){
+            case "0x000000ff":
+                mouseTarget.setFill(Color.RED);
+                break;
+            case "0xff0000ff":
+                mouseTarget.setFill(Color.BLUE);
+                break;
+            default:
+                mouseTarget.setFill(Color.BLACK);
+                break;
+        }
     }
 
-    private void aiSetColor(int cellX, int cellY){
-        int index = (cellX - 1) + (cellY - 1) * x;
-        ((Rectangle)root.getChildren().get(index)).setFill(Color.DARKGREEN);
+    public void aiSetColor(int cellX, int cellY){
+        //to make sure the algorithm never uses a wrong x or y
+        if(cellX <= x & cellX >= 0 & cellY <= y & cellY >= 0) {
+            //calculates the index of the given coordinates
+            int index = (cellX - 1) + (cellY - 1) * x;
+            ((Rectangle) root.getChildren().get(index)).setFill(Color.DARKGREEN);
+        }
+        else{
+            throw new NullPointerException();
+        }
     }
 
 
